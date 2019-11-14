feb16badd34b3bbbfc5bd0868f8d7f90d4393fb1
jwwei11 <j.wei@uchicago.edu>
2019-11-14 21:33:21 +0000

    add routes

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
+# https://github.com/appdev-projects/very-best-api
+# bRFaiXA6YExowcvgXfTGhzL5
+
+
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 Rails.application.routes.draw do
 
-
-
+  match("/cuisines", { :controller => "application", :action => "cuisines", :via => "get" })
+  match("/dishes", { :controller => "application", :action => "dishes", :via => "get" })
+  match("/dishes/:id", { :controller => "application", :action => "dishes_id", :via => "get" })
+  match("/neighborhoods", { :controller => "application", :action => "neighborhoods", :via => "get" })
+  match("/venues", { :controller => "application", :action => "venues", :via => "get" })
+  match("/venues/:id", { :controller => "application", :action => "venues_id", :via => "get" })
+  match("/users", { :controller => "application", :action => "users", :via => "get" })
+  match("/users/:id", { :controller => "application", :action => "users_id", :via => "get" })
+  match("/add_bookmark", { :controller => "application", :action => "add_bookmark", :via => "get" })
+  match("/users/:id/bookmarks", { :controller => "application", :action => "user_bookmarks", :via => "get" })
+  
+  match("/dises/:id/bookmarks", { :controller => "application", :action => "dishes_bookmark", :via => "get" })
+  match("/venues/:id/bookmarks", { :controller => "application", :action => "venues_bookmarks", :via => "get" })
+  match("/remove_bookmark/:id", { :controller => "application", :action => "remove_bookmark", :via => "get" })
+  match("/users/:id/bookmarked_dishes", { :controller => "application", :action => "users_bookmark_dishes", :via => "get" })
+  match("/dishes/:id/experts", { :controller => "application", :action => "dishes_experts", :via => "get" })
+  match("/venues/:id/specialties", { :controller => "application", :action => "venues_specialties", :via => "get" })
+  match("/venues/:id/fans", { :controller => "application", :action => "venues_fans", :via => "get" })
+  # /dishes and /venues with querystrings
+  match("/users/:id/bookmarks", { :controller => "application", :action => "users_bookmark_dishes", :via => "get" })
+  
   # ====================================================
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
